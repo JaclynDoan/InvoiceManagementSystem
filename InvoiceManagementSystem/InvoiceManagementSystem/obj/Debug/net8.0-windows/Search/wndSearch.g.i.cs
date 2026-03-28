dc396d5fc9b1f8a2cfee474b14b2f44a2239aaf8
Tayli Christensen121 <taylichristensen@mail.weber.edu>
2026-03-28 16:18:44 -0600

    Might have fixed the errors

--- InvoiceManagementSystem/InvoiceManagementSystem/obj/Debug/net8.0-windows/Search/wndSearch.g.i.cs
+++ InvoiceManagementSystem/InvoiceManagementSystem/obj/Debug/net8.0-windows/Search/wndSearch.g.i.cs
@@ -94,13 +94,13 @@ namespace InvoiceManagementSystem.Search {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.5.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/InvoiceManagementSystem;V1.0.0.0;component/search/wndsearch.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/InvoiceManagementSystem;component/search/wndsearch.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Search\wndSearch.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -110,7 +110,7 @@ namespace InvoiceManagementSystem.Search {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.5.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
